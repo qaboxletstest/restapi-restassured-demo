af9d0891011cf9fb468aad7944822e3125a16c0c
qaboxletstest <qaboxletstest@gmail.com>
2021-12-06 20:00:29 +0530

    XML Schema Validation Basics

---
+++ restapi-restassured-demo/src/test/java/basics/XMLSchemaValidation.java
@@ -0,0 +1,33 @@
+package basics;
+
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.http.Header;
+import io.restassured.matcher.RestAssuredMatchers;
+import io.restassured.specification.RequestSpecification;
+
+public class XMLSchemaValidation {
+	
+	RequestSpecification httpRequest;
+
+	@BeforeMethod
+	public void setUp() {
+		RestAssured.baseURI = "http://localhost:5002";
+		RestAssured.basePath = "/api/members/{id}";
+		Header acceptHeader = new Header("Accept", "application/xml");
+		httpRequest =  RestAssured.given().auth().basic("admin", "admin").header(acceptHeader).pathParam("id", 4);
+	}
+	
+	@Test(enabled = true)
+	public void singleMember() {
+
+		httpRequest
+				.when()
+					.get()
+				.then()
+					.assertThat()
+						.body(RestAssuredMatchers.matchesXsdInClasspath("XSD/singleMember.xsd"));
+	}
+}
